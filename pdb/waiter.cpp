a4ba3a27817af78d57ba8becf3dcc5d6cd22f485
cynicanonimus <me@amironov.com>
2013-03-17 21:56:28 +0100

    17.03.2013-4 see changes.txt

--- pdb/waiter.cpp
+++ pdb/waiter.cpp
@@ -11,7 +11,7 @@ Waiter::Waiter(QObject *parent) :
 void Waiter::wait (int i_sec)
 {
     //
-    QTime dieTime= QTime::currentTime().addSecs(2);
+    QTime dieTime= QTime::currentTime().addSecs(i_sec);
     while( QTime::currentTime() < dieTime )
     {
         QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
